2edc8557c8a673d00fa1c4bd07a4d606adbc2313
dickhead11 <46551498+dickhead11@users.noreply.github.com>
2019-01-14 15:22:44 +1100

    more changes

--- dracula_view.c
+++ dracula_view.c
@@ -29,7 +29,7 @@ typedef struct dracula_view {
 	int currPlayer;
 	Drac_Char ch[5];
 } dracula_view;
-
+//TO DO: needs editing!
 dracula_view *dv_new (char *past_plays, player_message messages[])
 {
 	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
@@ -41,7 +41,11 @@ dracula_view *dv_new (char *past_plays, player_message messages[])
 
 void dv_drop (dracula_view *dv)
 {
-	
+	int i = 0;
+	while (i < 5) {
+		free(dv->ch[i]);
+		i++;
+	}
 	free (dv);
 }
 
@@ -65,44 +69,71 @@ location_t dv_get_location (dracula_view *dv, enum player player)
 {
 	return dv->ch[player]->location;
 }
-
+//TO DO: needs editing!
 void dv_get_player_move (
 	dracula_view *dv, enum player player,
 	location_t *start, location_t *end)
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	//generate move data in dracview
 	return;
 }
-
+//TO DO: needs editing!
 void dv_get_locale_info (
 	dracula_view *dv, location_t where,
 	int *n_traps, int *n_vamps)
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	if (where != validLocation) {
+		&n_traps = 0;
+		&n_vamps = 0;//figure out shit
+	}
+
 	return;
 }
-
+//TO DO: needs editing!
 void dv_get_trail (
 	dracula_view *dv, enum player player,
 	location_t trail[TRAIL_SIZE])
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	int i = 0;
+	while (i < TRAIL_SIZE) {//generate move data in dracview
+		if (MOVE_NOT_PLAYED) {
+			trail[i] = -1;
+		}
+		else {	
+			trail[i] = MOVE;
+		}
+		i++;
+	}
 }
 
-
+//TO DO: needs editing!
 location_t *dv_get_dests (
 	dracula_view *dv, size_t *n_locations, bool road, bool sea)
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	*n_locations = 0;
-	return NULL;
+	location_t arr[&n_locations];
+	if (road == true) {
+		// find all road connections for dracula and add to arr[]
+	}
+	if (sea == true) {
+		// find all sea connections for dracula and add to arr[]
+	}
+	return arr;
 }
-
+//TO DO: needs editing!
 location_t *dv_get_dests_player (
 	dracula_view *dv, size_t *n_locations, enum player player,
 	bool road, bool rail, bool sea)
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	*n_locations = 0;
-	return NULL;
+	if (player == PLAYER_DRACULA) return dv_get_dests(dv, n_locations, road, sea);
+	location_t arr[&n_locations];
+	if (road == true) {
+		// find all road connections for player and add to arr[]
+	}
+	if (sea == true) {
+		// find all sea connections for player and add to arr[]
+	}
+	if (rail == true) {
+		// find all rail connections for player and add to arr[]
+	}
+	return arr;
 }
