f38a55dd43253bb3d80a085088cfe3db5ce033aa
ghostale <29471989+ghostale@users.noreply.github.com>
2019-01-15 17:36:57 +1100

    Add files via upload
    Cleaner version -- last function incomplete

--- hunter_view.c
+++ hunter_view.c
@@ -17,17 +17,18 @@
 #include "game.h"
 #include "game_view.h"
 #include "hunter_view.h"
-// #include "map.h" ... if you decide to use the Map ADT
+#include "map.h" 
 
 typedef struct characters {
 	int health;
-	location_t location;
+	location_t location;// Curr location
+	location_t trail[TRAIL_SIZE]; // Last 6
 } characters;
 
 typedef characters *Characters;
 
 typedef struct hunter_view {
-	int turn;
+	round_t round;
 	int score;
 	
 	int curr_player;
@@ -40,42 +41,25 @@ hunter_view *hv_new (char *past_plays, player_message messages[])
 	if (new == NULL) err (EX_OSERR, "couldn't allocate HunterView");
 
 	// Following put into hunter_view
-	int turn = 1;
-	int score = 366;
-	int vampire = 1; // True/false for immature vampire
+	int turn = 0;
+	int score = GAME_START_SCORE;
+	int vampire = 0; // True/false for immature vampire
 	
 	// Following used for every turn
 	char location[2];
 	char prevloc[2] = { '0', '0' }; // Hunter's prev location
 	int player = -1; // To keep compiler happy
-	for (int i = 0; i < 4; i++) new->hts[i]->health = 9; // All hts begin w/ 9 HP
+	for (int i = 0; i < 4; i++)
+	{
+		new->hts[i]->health = GAME_START_HUNTER_LIFE_POINTS;
+		for (int j = 0; j < TRAIL_SIZE; j++) new->hts[i]->trail[j] = -1; // Also initialise h. trails
+	}
 
 	int n = 1; // Char count
 	for (char *c = past_plays; *c != '\0'; c++)
 	{
 		switch (n % 8) // Read past_plays byte by byte b4 fully initialising hunter_view
 		{
-			case (0) :
-				turn++; // ' ' = turn in past_plays
-				score--; // Happens after dracula finishes his turn
-				
-				if (turn % 13 == 1) vampire = 1; // D left behind immature v.
-				else if (vampire) vampire++;
-				if (vampire == 7) { score -= 13; vampire = 0; } // V. matures
-				
-				if (player != PLAYER_DRACULA)
-				{
-					new->hts[player]->location = location_find_by_abbrev (location); // Update hunters' locations
-					if (new->hts[player]->health <= 0)
-					{
-						new->hts[player]->health = 9;
-						location[0] = 'J'; location[1] = 'M'; // Htr teleported to hospital
-					}
-				}
-				
-				prevloc[0] = location[0]; prevloc[1] = location[1]; // Redefine prev location
-
-				break;
 			case (1) :
 				switch (*c)
 				{
@@ -95,34 +79,61 @@ hunter_view *hv_new (char *past_plays, player_message messages[])
 						player = PLAYER_DRACULA;
 				}
 				break;
+			
 			case (2) :
 				location[0] = *c;
 				break;
+			
 			case (3) :
 				location[1] = *c;
                                 if (!strcmp (location, prevloc))
-					new->hts[player]->health += 3; // If hts rested in city
+					new->hts[player]->health += LIFE_GAIN_REST; // If hts rested in city
 				if (new->hts[player]->health > 9) new->hts[player]->health = 9; // Retain HP cap
 
 				break;
+			
+			case (0) :
+                                turn++; // ' ' = turn in past_plays
+                                score -= SCORE_LOSS_DRACULA_TURN; // Happens after dracula finishes his turn
+
+                                if ((turn / 5) % 13 == 0 &&
+                                                player == PLAYER_DRACULA && location[0] == 'C') vampire = 1; // D left behind immature v.
+                                else if (vampire) vampire++;
+                                if (vampire == 7) { score -= SCORE_LOSS_VAMPIRE_MATURES; vampire = 0; } // V. matures
+
+                                if (player != PLAYER_DRACULA)
+                                {
+                                        if (new->hts[player]->health <= 0)
+                                        {
+                                                new->hts[player]->health = 9;
+                                                location[0] = 'J'; location[1] = 'M'; // Htr teleported to hospital
+                                        }
+                                        new->hts[player]->location = location_find_by_abbrev (location); // Update hunters' locations
+                                        new->hts[player]->trail[(turn / 5) % 6] = location_by_abbrev (location); // Update trail, overwriting oldest entry if full
+                                }
+
+                                prevloc[0] = location[0]; prevloc[1] = location[1]; // Redefine prev location
+
+                                break;
+			
 			default :
                                 if (player == PLAYER_DRACULA) break;
 				
-				if (*c == 'T') new->hts[player]->health -= 2;
-				if (new->hts[player]->health <= 0) score -= 6;
+				if (*c == 'T') new->hts[player]->health -= LIFE_LOSS_TRAP_ENCOUNTER;
+				if (new->hts[player]->health <= 0) score -= SCORE_LOSS_HUNTER_HOSPITAL;
 				
 				if (*c == 'V') vampire = 0;
 				
-				if (*c == 'D') new->hts[player]->health -= 4;
-				if (new->hts[player]->health <= 0) score -= 6;
+				if (*c == 'D') new->hts[player]->health -= LIFE_LOSS_DRACULA_ENCOUNTER;
+				if (new->hts[player]->health <= 0) score -= SCORE_LOSS_HUNTER_HOSPITAL;
 		}
 
 		
 		n++;
 	}
 
-	new->curr_player = ((turn - 1) % 5);
-	new->turn = turn;
+	new->curr_player = ((turn + 1) % 5); // +1 as curr_plyr is yet to make a move
+	new->round = turn / 5;
 	new->score = score;
 
 	return new;
@@ -130,61 +141,55 @@ hunter_view *hv_new (char *past_plays, player_message messages[])
 
 void hv_drop (hunter_view *hv)
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	for (int i = 0; i < 4; i ++) free (hts[i]);
 	free (hv);
 }
 
 round_t hv_get_round (hunter_view *hv)
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return 0;
+	return hv->round;
 }
 
 enum player hv_get_player (hunter_view *hv)
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return 0;
+	return hv->curr_player;
 }
 
 int hv_get_score (hunter_view *hv)
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return 0;
+	return hv->score;
 }
 
 int hv_get_health (hunter_view *hv, enum player player)
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return 0;
+	return hv->hts[player]->health;
 }
 
 location_t hv_get_location (hunter_view *hv, enum player player)
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return 0;
+	return hv->hts[player]->location;
 }
 
 void hv_get_trail (
 	hunter_view *hv, enum player player,
 	location_t trail[TRAIL_SIZE])
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	for (int i = 0; i < 6; i++) trail[i] = hv->hts[player]->trail[i];
+	return;
 }
 
 location_t *hv_get_dests (
 	hunter_view *hv, size_t *n_locations,
 	bool road, bool rail, bool sea)
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	*n_locations = 0;
-	return NULL;
+	return hv_get_dests_player (hv, n_locations, hv->curr_player, road, rail, sea);
 }
 
 location_t *hv_get_dests_player (
 	hunter_view *hv, size_t *n_locations, enum player player,
 	bool road, bool rail, bool sea)
 {
-	/// @todo REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	
 	*n_locations = 0;
 	return NULL;
 }
