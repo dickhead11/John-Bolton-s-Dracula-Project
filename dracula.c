323b8affefa886adf147bf2f27b35f4610e44c22
dickhead11 <46551498+dickhead11@users.noreply.github.com>
2019-01-29 14:39:59 +1100

    Update dracula.c

--- dracula.c
+++ dracula.c
@@ -10,14 +10,12 @@
 #include "dracula_view.h"
 #include "game.h"
 #include <string.h>
-
 void decide_dracula_move (DraculaView dv)
 {	
 	size_t num_locations = 0;
-	location_t *yo = dv_get_dests(
-	dv, &numLocations, true, true);
+	location_t *yo = dv_get_dests(dv, &numLocations, true, true);
 
-	for (int i = 0, i < 4, i++) {
+	for (int i = 0; i < 4; i++) {
 		location_t g = dv_get_location(dv, i);
 		if ((strcmp(location_get_abbrev (g), "KL") == 0) || 
 			strcmp(location_get_abbrev (g), "GA") == 0)) {
@@ -29,7 +27,36 @@ void decide_dracula_move (DraculaView dv)
 		}
 	}
 	if (dv_get_health(dv, 4) < 3) register_best_play ("CD", "coup averted");
-	location_t *gay = dv_get_dests(dv, num_locations, true, true);
-	const char *meh = location_get_abbrev (gay[0]);
-	register_best_play (meh, "tricked");
+	char reachableLocHunters[100];
+	int j = 0;
+	for (int i = 0; i < 4; i++) {
+		location_t g = dv_get_location(dv, i);
+		int z = (i + dv_get_round(DraculaView dv)) % 4;
+		location_t *hunterLocations = reachable_locations 
+			(connections, &num_locations, g, false, z, true, true);
+		int numPotentialLoc = strlen(hunterLocations);
+		for (int h = 0, j; h <= numPotentialLoc; j++, h++) {
+			strcpy(reachableLocHunter[j], hunterLocations[h]);
+		}
+	}
+	yo = dv_get_dests(dv, &num_Locations, true, true);
+	// code that moves dracula to the first location that is unreachable to the hunters
+	// if no unreachable location, flies to the castle
+	for (int i = 0; i < numLocations; i++); {
+		for (int z = 0; z < strlen(reachableLocHunter), z++) {
+			if (strcmp(yo[i], reachableLocHunter[z]) == 0) break;
+			if (z == strlen(reachableLocHunter) - 1) {
+				register_best_play (location_get_abbrev (yo[i]), "tricked");
+				return;
+			}
+		}
+		if (i == numLocations - 1;) {
+			register_best_play ("CD", "coup averted");
+			return;
+		}
+	}
+	// random move
+	/*location_t *gay = dv_get_dests(dv, num_locations, true, true);
+	const char *meh = location_get_abbrev (&gay[0]);
+	register_best_play (meh, "tricked"); */
 }
