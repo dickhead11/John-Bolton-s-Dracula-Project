eaad58e344bcb1476422a9f726b85b55df0e076f
dickhead11 <46551498+dickhead11@users.noreply.github.com>
2019-01-29 23:40:36 +1100

    Update dracula.c

--- dracula.c
+++ dracula.c
@@ -11,16 +11,18 @@
 #include "game.h"
 #include <string.h>
 #include "map.h"
+#include "places.h"
 
 void decide_dracula_move (DraculaView dv)
 {	
+	Map map = map_new ();
 	size_t numLocations = 0;
 	location_t *yo = dv_get_dests(dv, &numLocations, true, true);
 
 	for (int i = 0; i < 4; i++) {
 		location_t g = dv_get_location(dv, i);
 		if ((strcmp(location_get_abbrev (g), "KL") == 0) || 
-			strcmp(location_get_abbrev (g), "GA") == 0) {
+			(strcmp(location_get_abbrev (g), "GA") == 0)) {
 			break;
 			if (dv_get_health(dv, 4) <= 16 && i == 3) { 
 				register_best_play ("CD", "coup averted");
@@ -30,49 +32,46 @@ void decide_dracula_move (DraculaView dv)
 	}
 	if (dv_get_health(dv, 4) < 3) register_best_play ("CD", "coup averted");
 	// code that gathers all reachable locations to hunters into a single array
-	location_t reachableLocHunters[100];
-	int j = 0;
+	location_t reachableLocHunter[100];
+	int j = 0, numPotentialLoc;
 	for (int i = 0; i < 4; i++) {
 		location_t g = dv_get_location(dv, i);
 		int z = (i + dv_get_round(dv)) % 4;
-		Map connections = map_new ();
-		location_t *hunterLocations = reachable_locations 
-			(connections, &numLocations, g, false, z, true, true);
-		int numPotentialLoc = (int) numLocations;
-		for (int h = 0; h < numPotentialLoc; j++, h++) {
-			(reachableLocHunters[j] = hunterLocations[h]);
+		location_t *hunterLocations = reachable_locations (map, &numLocations, g, false, z, true, true);
+		numPotentialLoc = (int) numLocations;
+		for (int h = 0, j; h < numPotentialLoc; j++, h++) {
+			reachableLocHunter[j] = hunterLocations[h];
 		}
 	}
 	// code that moves dracula to the first location that is unreachable to the hunters
 	// if no unreachable location, flies to the castle if castle is not surrounded
 	yo = dv_get_dests(dv, &numLocations, true, true);
-	for (int i = 0; i < (int) numLocations; i++) {
-		for (int z = 0; z < (int) strlen(reachableLocHunters); z++) {
-			if (strcmp(yo[i], reachableLocHunters[z]) == 0) break;
-			if (z == (int) strlen(reachableLocHunters) - 1) {
+	for (int i = 0; i < numLocations; i++) {
+		for (int z = 0, i; z < numPotentialLoc; z++) {
+			if (yo[i] == reachableLocHunter[z]) break;
+			if (z == numPotentialLoc - 1) {
 				register_best_play (location_get_abbrev (yo[i]), "too rich to be caught");
 				return;
 			}
 		}
 		if (i == (int) numLocations - 1) {
-			for (int k = 0; k < 4; k++) {
-				location_t g = dv_get_location(dv, k);
+			for (int i = 0; i < 4; i++) {
+				location_t g = dv_get_location(dv, i);
 				if ((strcmp(location_get_abbrev (g), "KL") == 0) || 
-				strcmp(location_get_abbrev (g), "GA") == 0) {
+				(strcmp(location_get_abbrev (g), "GA") == 0))
 				break;
 				if (i == 3) { 
 					register_best_play ("CD", "coup averted");
 					return;
-				    }
-		        }
-		    }
+				}
+			}
 		// we are surrounded, but hunters are camping outside castle dracula 
 		// and we still have plenty of blood points, so just make a normal move
 			register_best_play (location_get_abbrev (yo[0]), "too rich to be caught");
 		}
 	}
 	// random move
-	/*location_t *gay = dv_get_dests(dv, num_locations, true, true);
-	const char *meh = location_get_abbrev (&gay[0]);
+	/*location_t *gay = dv_get_dests(dv, &num_locations, true, true);
+	char *meh = location_get_abbrev (&gay[0]);
 	register_best_play (meh, "tricked"); */
 }
